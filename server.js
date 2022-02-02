0e7f84dc0949f86b5d7cae342cc722867985f13e
Yousef-Abu-Qatrieh <yousef_1995q@hotmail.com>
2022-02-02 05:55:46 +0200

    update

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios')
+const pg = require('pg');
 const data = require('./Movie Data/data.json')
 const PORT = process.env.PORT
 const server = express();
+const client = new pg.Client(process.env.DATABASE_URL)
 
 
 server.use(cors());
+server.use(express.json())
 server.get('/', handelGet)
 server.get('/favorite', handleGetFavorite)
 server.get('/trending', handleTrending)
 server.get('/search', handelSearch)
+server.post('/addMovie', addMovieHandler)
+server.get('/getMovies', handleGetMovies)
 server.use('*', handleNotFound)
 server.use(handleServerErorr)
 
@@ -57,6 +62,7 @@ function handleTrending(req, res) {
 
 
         }).catch((err) => {
+            handleServerErorr(error,req,res)
 
         })
 
@@ -69,17 +75,39 @@ function handelSearch(req, res) {
         .then((ren) => {
             // console.log(ren.data.results);
 
-            let searchs = ren.data.results.map(search =>{
-                return new Trending(search.id,search.title,search.release_date,search.poster_path,search.overview);
+            let searchs = ren.data.results.map(search => {
+                return new Trending(search.id, search.title, search.release_date, search.poster_path, search.overview);
             });
-            res.status(200).json(searchs);  
+            res.status(200).json(searchs);
 
 
         }).catch((err) => {
+            handleServerErorr(error,req,res)
 
         })
 }
 
+function addMovieHandler(req,res){
+    const movie = req.body;
+       console.log(movie)
+      let sql = `INSERT INTO movies(title,release_date,poster_path,overview) VALUES ($1,$2,$3,$4) RETURNING *;`
+      let values=[movie.title,movie.release_date,movie.poster_path,movie.overview];
+      client.query(sql,values).then(data =>{
+          res.status(200).json(data.rows);
+      }).catch(error=>{
+        handleServerErorr(error,req,res)
+      });
+
+
+}
+function handleGetMovies(req,res){
+    let sql = `SELECT * FROM movies;`;
+    client.query(sql).then(data=>{
+       res.status(200).json(data.rows);
+    }).catch(error=>{
+        handleServerErorr(error,req,res)
+    });
+}
 
 
 function handleGetFavorite(req, res) {
@@ -97,7 +125,9 @@ function handleNotFound(req, res) {
     return res.status(404).send("page not found error")
 
 }
-server.listen(PORT, () => {
-    console.log(`listen to port ${PORT}`);
 
+client.connect().then(() => {
+    server.listen(PORT, () => {
+        console.log(`listining to port ${PORT}`)
+    })
 })
